09bfba8379d3181dc9b1c904efed94d186650d88
Bob <bob@192-168-1-10.tpgi.com.au>
2020-04-07 12:37:38 +1000

    New ng-employee project

--- src/App.js
+++ src/App.js
@@ -69,6 +69,7 @@ const projects = [
   { name: 'poker', description: 'Draw 5 cards then check poker hands', demoUrl: 'http://www.bobsyd.com/demo/poker/', codeUrl: 'https://github.com/wanbo-xiao/poker' },
   { name: 'tic-tac-toe', description: 'tic-tac-toe game', demoUrl: 'http://www.bobsyd.com/demo/tic-tac-toe/', codeUrl: 'https://github.com/wanbo-xiao/tic-tac-toe' },
   { name: 'twitter-search', description: 'A search page for twitter', demoUrl: 'http://www.bobsyd.com/demo/twitter-search/', codeUrl: 'https://github.com/wanbo-xiao/twitter-search' },
+  { name: 'ng-employee', description: 'Employees management', demoUrl: 'http://www.bobsyd.com/demo/ng-employee/', codeUrl: 'https://github.com/wanbo-xiao/ng-employees' },
 ];
 
 export default function Album() {
